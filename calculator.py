0545c1c915facb8f5f46c7f9dd67cd8aed70fb02
DESKTOP-BFISF3G\maa sharda <adityaverna4769@gmail.com>
2022-10-10 21:52:53 +0530

    Calculator

---
+++ calculator.py
@@ -0,0 +1,19 @@
+first = int(input("Enter Your First Number: "))
+sign = input("Enter Your sign, [+, -, *, /, //, %]: ")
+second = int(input("Enter Your Second Number: "))
+
+
+if sign=="+" :
+    print(first+second)
+elif sign=="-":
+    print(first-second)
+elif sign=="*":
+    print(first*second)
+elif sign=="/":
+    print(first/second)
+elif sign=="//":
+    print((first//sign))
+elif sign=="%":
+    print(first%second)
+else:
+    print("Invalid Number...")
