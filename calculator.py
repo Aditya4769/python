60a4d06afe5ff6ebf3887a8e908d0caf4c60821a
Aditya4769 <114800003+Aditya4769@users.noreply.github.com>
2022-10-11 19:56:07 +0530

    Calculator

--- calculator.py
+++ calculator.py
@@ -1,17 +1,38 @@
-first = int(input("Enter Your First Number: "))
-sign = input("Enter Your sign, [+, -, *, /, %]: ")
-second = int(input("Enter Your Second Number: "))
+#Taking a Input by user.
+x = int(input("Enter Your First Number: "))
+y = int(input("Enter Your Second Number: "))
 
+#Taking an operators
+operator= input("Enter Your operator, [+, -, *, /, %, **, //]: ")
 
-if sign=="+" :
-    print(first+second)
-elif sign=="-":
-    print(first-second)
-elif sign=="*":
-    print(first*second)
-elif sign=="/":
-    print(first/second)
-elif sign=="%":
-    print(first%second)
+#if and elif condition.
+
+#For Adding Two Numbers.
+if operator=="+" :
+    print(x+y)
+
+#For subtracting Two Number.
+elif operator=="-":
+    print(x-y)
+
+#For multiplication of two Number.
+elif operator=="*":
+    print(x*y)
+
+#For Divide Two Numbers.
+elif operator=="/":
+    print(x/y)
+
+#For Getting a reminder Value.
+elif operator=="%":
+    print(x%y)
+
+#For find an exponential form.
+elif operator=="**":
+    print(x**y)
+
+#For getting a Quotient Value Without any decimal point.
+elif operator=="//":
+    print(x//y)
 else:
     print("Invalid Number...")
