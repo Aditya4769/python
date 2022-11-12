82ffbed00663b053ad2ac626cf6952adf06b497e
Aditya4769 <114800003+Aditya4769@users.noreply.github.com>
2022-11-12 16:52:03 +0530

    python

--- reverse.py
+++ reverse.py
@@ -1,3 +1,16 @@
-numbers=[1,2,3,4,5,6,7,8,9,10]
-numbers.reverse()
-print(numbers)
+# numbers=[1,2,3,4,5,6,7,8,9,10]
+# numbers.reverse()
+# print(numbers)
+# num2 = list(numbers)
+# print(num2)
+
+list1 = ["x","y","z"]
+list2 = [1,2,3]
+# list3 = list1+list2
+# list1.extend(list2)
+# print(list3)
+# print(list1)
+for x in list2:
+
+    list1.append(x)
+print(list1)
